bc575c5bb4b0f96580bf3083b61904b3bac293fe
SamarthGlitch <samarthsajith123@gmail.com>
2025-09-09 18:46:08 +0530

    Introduction-to-python

---
+++ introduction/keywords.py
@@ -0,0 +1,4 @@
+print("Welcome to the world of programming")
+print("\n keywords in python are")
+import keyword
+print(keyword.kwlist)
---
+++ introduction/print.py
@@ -0,0 +1,6 @@
+print("Hello This is the First class of Python")
+print(123)
+print("Hello world", 123)
+print(1 + 2 + 3 + 4)
+print(4*7)
+#This is a comment
