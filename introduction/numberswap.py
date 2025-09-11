7c6d2d50e831855723d5d79c7dd70e83d1cd1e48
SamarthGlitch <samarthsajith123@gmail.com>
2025-09-11 18:48:52 +0530

    datatypes

---
+++ introduction/numberswap.py
@@ -0,0 +1,9 @@
+x = int(input("Enter the value of x: "))
+y = int(input("Enter the value of y: "))
+
+temp = x
+x = y
+y = temp
+
+print("Value of x after swapping: ", x)
+print("Value of y after swapping: ", y)
---
+++ introduction/strings.py
@@ -0,0 +1,15 @@
+first_name = "Codingal"
+last_name = "Educations"
+full_name = first_name + " " + last_name
+example="Haa"*5
+
+print("First Name: ", first_name)
+print("Last Name: ", last_name)
+print("Full Name: ", full_name)
+print("String multiplied 5 times gives the result: ", example)
+
+word ="Coding"
+print("Length of the String: ", len(word))
+print("First letter of the String: ", word[0])
+print("Last letter of the String: ", word[5])
+print("String Sliced: ", word[0:3])
