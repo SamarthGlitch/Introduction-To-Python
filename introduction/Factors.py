78c781af5a0b1e200cf424c7a445edec2339bfc8
SamarthGlitch <samarthsajith123@gmail.com>
2025-12-02 18:28:49 +0530

    Math

---
+++ introduction/Factors.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number to find its factors: "))
+def print_factors(number):
+    print("The factors of ",number," are: ")
+    for i in range(1, number + 1):
+        if number % i == 0:
+            print(i)
+
+print_factors(number)
