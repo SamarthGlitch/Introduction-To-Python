68ba417ddfde5971c82a06882e44ceab2338f79e
SamarthGlitch <samarthsajith123@gmail.com>
2025-12-25 18:24:45 +0530

    powers

---
+++ introduction/clas1.py
@@ -0,0 +1,14 @@
+"""Create a class BankAccount with:
+
+name, balance
+
+methods: deposit(), withdraw(), display_balance()"""
+class BankAccount():
+    def deposit():
+        pass
+    def withdraw():
+        pass
+    def display_balance():
+        pass
+
+name 
