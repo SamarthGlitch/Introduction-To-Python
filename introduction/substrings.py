52749e1cb6e42be1eaa1ee61de95a84a80773510
SamarthGlitch <samarthsajith123@gmail.com>
2026-05-19 18:31:38 +0530

    substrings

--- introduction/substrings.py
+++ introduction/substrings.py
@@ -1,6 +1,13 @@
-s = input("Enter string: ")
-n = len(s)
+str1 = input("Enter string : ")
+def printPowerSet(arr, set_size):
+    power_set_size = 2**set_size
 
-for i in range(n):
-    for j in range(i + 1):
-        print(s[j:i+1])
+    for outer in range(power_set_size):
+        subset = ""
+        for inner in range(set_size):
+            if outer & (1 << inner):
+                subset += str(arr[inner])
+        print(subset.strip())
+
+arr = list(str1)
+printPowerSet(arr, len(arr))
